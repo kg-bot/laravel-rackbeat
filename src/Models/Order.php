3a8e71438922a4386948a8ca7ab7854c29d87b06
Stefan Ninic <ninicstefan94@gmail.com>
2020-01-24 17:44:37 +0100

    Fixed book on convert

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -70,7 +70,7 @@ class Order extends Model
     {
         return $this->request->handleWithExceptions(function () use ($book) {
 
-            $response = json_decode((string)$this->request->client->post("{$this->entity}/{$this->{$this->primaryKey}}/convert-to-invoice" . (($book === true) ? '?book' : ''))
+            $response = json_decode((string)$this->request->client->post("{$this->entity}/{$this->{$this->primaryKey}}/convert-to-invoice?book=" . (($book === true) ? 'true' : 'false'))
                 ->getBody());
 
             $invoice = (new CustomerInvoiceBuilder($this->request))->find($response->invoice_id);
