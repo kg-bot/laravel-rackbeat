75e03224454ecce7da572bfb8b57be79d133e86b
Stefan Ninic <ninicstefan94@gmail.com>
2020-02-07 12:38:42 +0100

    Send email body when booking customer invoice

--- src/Models/CustomerInvoice.php
+++ src/Models/CustomerInvoice.php
@@ -17,11 +17,47 @@ class CustomerInvoice extends Model
     protected $entity = 'customer-invoices';
     protected $primaryKey = 'number';
 
+    /**
+     * Book customer invoice
+     *
+     * @param bool|array $send_email [optional] <p>
+     * This parameter indicates should the mail be sent to customer,
+     * if you send it as array it must be in format like this:
+     * [
+     *     "send" => false,
+     *     "body" => "Something",
+     *     "subject" => "YOUR SUBJECT",
+     *     "receivers" => [
+     *         "to" => [ "email@here.com"],
+     *         "cc" => ["cc-email@here.com"],
+     *         "bcc" => ["bcc1-email@here.com", "bcc2@here.com"]
+     *     ]
+     * ]
+     * If you set send to false or use ->book(true) email body won't be included
+     * </p>
+     * @return mixed
+     * @throws \Rackbeat\Exceptions\RackbeatClientException
+     * @throws \Rackbeat\Exceptions\RackbeatRequestException
+     */
     public function book($send_email = false)
     {
         return $this->request->handleWithExceptions(function () use ($send_email) {
 
-            return $this->request->client->post("{$this->entity}/{$this->{$this->primaryKey}}/book" . (($send_email === true) ? '?send_mail=true' : ''))
+            $query = '';
+            $body = [];
+
+            if (is_bool($send_email)) {
+
+                $query = ($send_email === true) ? '?send_mail=true' : '';
+            } else if (is_array($send_email)) {
+
+                $body = [
+
+                    'mail' => $send_email,
+                ];
+            }
+
+            return $this->request->client->post("{$this->entity}/{$this->{$this->primaryKey}}/book" . $query, $body)
                 ->getBody()
                 ->getContents();
         });
