033a927a778f1b2db6c1a79f4f03636bfd839f9a
Stefan Ninic <ninicstefan94@gmail.com>
2020-06-16 13:17:20 +0200

    New methods in DraftOrder

--- src/Models/DraftOrder.php
+++ src/Models/DraftOrder.php
@@ -9,20 +9,42 @@
 namespace Rackbeat\Models;
 
 
+use Rackbeat\Builders\OrderNoteBuilder;
+use Rackbeat\Builders\OrderShipmentBuilder;
 use Rackbeat\Utils\Model;
 
 class DraftOrder extends Model
 {
-    protected $entity     = 'orders/drafts';
-    protected $primaryKey = 'number';
+	protected $entity     = 'orders/drafts';
+	protected $primaryKey = 'number';
 
-    public function getPDF()
-    {
-        return $this->request->handleWithExceptions( function () {
-            $response = $this->request->client->get("{$this->entity}/{$this->url_friendly_id}.pdf");
+	public function getPDF() {
+		return $this->request->handleWithExceptions( function () {
+			$response = $this->request->client->get( "{$this->entity}/{$this->url_friendly_id}.pdf" );
 
 
-            return json_decode((string)$response->getBody());
-        } );
-    }
+			return json_decode( (string) $response->getBody() );
+		} );
+	}
+
+	public function shipments() {
+		return $this->request->handleWithExceptions( function () {
+
+			$builder = new OrderShipmentBuilder( $this->request );
+
+			return $builder->get( [
+				[ 'order_number', '=', $this->url_friendly_id ],
+			] );
+		} );
+	}
+
+	public function notes() {
+		return $this->request->handleWithExceptions( function () {
+
+			$builder = new OrderNoteBuilder( $this->request );
+			$builder->setEntity( str_replace( ':number', $this->url_friendly_id, $builder->getEntity() ) );
+
+			return $builder->get();
+		} );
+	}
 }
