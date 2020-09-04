0889e72f82fe0cdf463738911f934a0f8d20d56a
Stefan Ninic <ninicstefan94@gmail.com>
2020-09-04 14:52:33 +0200

    json_decode() if html is no required

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -9,112 +9,111 @@
 namespace Rackbeat\Models;
 
 
+use Illuminate\Support\Arr;
 use Rackbeat\Utils\Model;
 
 class Product extends Model
 {
-    public    $number;
-    protected $entity     = 'products';
-    protected $primaryKey = 'number';
+	public    $number;
+	protected $entity     = 'products';
+	protected $primaryKey = 'number';
 
-    public function inventoryMatrix( $location_id = null, array $filter = null )
-    {
-        return $this->request->handleWithExceptions( function () use ( $location_id, $filter ) {
+	public function inventoryMatrix( $location_id = null, array $filter = null ) {
+		return $this->request->handleWithExceptions( function () use ( $location_id, $filter ) {
 
-            $query = '';
+			$query = '';
 
-            // We need to use location filter if user has provided any
-            if ( !is_null( $location_id ) ) {
+			// We need to use location filter if user has provided any
+			if ( !is_null( $location_id ) ) {
 
-                $query .= '?location_id=' . $location_id;
-            }
+				$query .= '?location_id=' . $location_id;
+			}
 
-            if ( !is_null( $filter ) ) {
+			if ( !is_null( $filter ) ) {
 
-                foreach ( $filter as $parameter => $value ) {
+				foreach ( $filter as $parameter => $value ) {
 
-                    if ( $query === '' ) {
+					if ( $query === '' ) {
 
-                        $query .= '?' . $parameter . '=' . $value;
+						$query .= '?' . $parameter . '=' . $value;
 
-                    } else {
+					} else {
 
-                        $query .= '&' . $parameter . '=' . $value;
-                    }
-                }
-            }
+						$query .= '&' . $parameter . '=' . $value;
+					}
+				}
+			}
 
-            $response = $this->request->client->get("{$this->entity}/{$this->url_friendly_id}/variation-matrix" .
-                $query);
-
-
-            return json_decode((string)$response->getBody());
-        } );
-
-    }
-
-    public function variations( $variation_id = 1001 )
-    {
-        return $this->request->handleWithExceptions( function () use ( $variation_id ) {
-
-            $response = $this->request->client->get("variations/{$this->url_friendly_id}/variation-matrix");
+			$response = $this->request->client->get( "{$this->entity}/{$this->url_friendly_id}/variation-matrix" .
+			                                         $query );
 
 
-            return json_decode((string)$response->getBody());
-        } );
-    }
+			$isHtml   = filter_var( Arr::get( $filter, 'html', false ), FILTER_VALIDATE_BOOLEAN );
+			$response = (string) $response->getBody();
 
-    public function location($number = null)
-    {
-        return $this->request->handleWithExceptions(function () use ($number) {
+			return $isHtml ? $response : json_decode( $response );
+		} );
 
-            $response = $this->request->client->get("{$this->entity}/{$this->url_friendly_id}/locations" . (($number !== null) ? '/' . $number : ''));
+	}
 
+	public function variations( $variation_id = 1001 ) {
+		return $this->request->handleWithExceptions( function () use ( $variation_id ) {
 
-            $response = json_decode((string)$response->getBody());
-
-            if (isset($response->product_locations)) {
-
-                return collect($response->product_locations);
-            } else if (isset($response->product_location)) {
-
-                return $response->product_location;
-            } else {
-
-                return $response;
-            }
+			$response = $this->request->client->get("variations/{$this->url_friendly_id}/variation-matrix");
 
 
-        } );
-    }
+			return (string) $response->getBody();
+		} );
+	}
 
-    /**
-     * Show reporting ledger for desired product https://app.rackbeat.com/reporting/ledger/{product_number}
-     * API docs: https://rackbeat.docs.apiary.io/#reference/inventory-reports/show
-     */
-    public function ledger()
-    {
-        return $this->request->handleWithExceptions( function () {
+	public function location($number = null) {
+		return $this->request->handleWithExceptions(function () use ($number) {
 
-            $response =
-                $this->request->client->get("reports/ledger/{$this->{ $this->primaryKey } }");
+			$response = $this->request->client->get("{$this->entity}/{$this->url_friendly_id}/locations" . (($number !== null) ? '/' . $number : ''));
 
 
-            return collect(json_decode((string)$response->getBody())->ledger_items);
+			$response = json_decode((string)$response->getBody());
 
-        } );
-    }
+			if (isset($response->product_locations)) {
 
-    public function fields()
-    {
+				return collect($response->product_locations);
+			} else if (isset($response->product_location)) {
 
-        return $this->request->handleWithExceptions( function () {
+				return $response->product_location;
+			} else {
 
-            $response = $this->request->client->get("{$this->entity}/{$this->url_friendly_id}/fields");
+				return $response;
+			}
 
 
-            return collect(json_decode((string)$response->getBody())->field_values);
+		} );
+	}
 
-        } );
-    }
+	/**
+	 * Show reporting ledger for desired product https://app.rackbeat.com/reporting/ledger/{product_number}
+	 * API docs: https://rackbeat.docs.apiary.io/#reference/inventory-reports/show
+	 */
+	public function ledger() {
+		return $this->request->handleWithExceptions( function () {
+
+			$response =
+				$this->request->client->get("reports/ledger/{$this->{ $this->primaryKey } }");
+
+
+			return collect(json_decode((string)$response->getBody())->ledger_items);
+
+		} );
+	}
+
+	public function fields() {
+
+		return $this->request->handleWithExceptions( function () {
+
+			$response = $this->request->client->get("{$this->entity}/{$this->url_friendly_id}/fields");
+
+
+			return collect(json_decode((string)$response->getBody())->field_values);
+
+		} );
+	}
 }
