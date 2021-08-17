1a8f5c8ba4e0ec2f1b790717f9625571bf9a8e86
Aleksandar <mitevskialeksandar@outlook.com>
2021-08-17 16:08:45 +0200

    removed line

--- src/Builders/WebhookBuilder.php
+++ src/Builders/WebhookBuilder.php
@@ -16,15 +16,8 @@ class WebhookBuilder extends Builder
     protected $entity = 'webhooks';
     protected $model  = Webhook::class;
 
-    /**
-    *
-    * @param int $id
-    *      
-    * */
-    public function deleteWebhook($id)
+    public function deleteWebhook(int $id)
     {
-        $id = rawurlencode( rawurlencode( $id ) );
-
         return $this->request->handleWithExceptions( function () use ( $id ) {
             $response     = $this->request->client->delete( "{$this->entity}/{$id}" );
         } );
